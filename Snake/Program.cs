2b5bd0d77ba5e2c6e1ebce025973b3149c133c6e
Superstas91 <superstas91@gmail.com>
2020-05-05 13:21:12 +0300

    game over

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -47,7 +47,30 @@ namespace Snake
 					snake.HandleKey(key.Key);
 				}
 			}
+			WriteGameOver();
+			Console.ReadLine();
+
 		}
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Евгений Картавец и Помельников СВ", xOffset - 5, yOffset++);
+			WriteText("на основе материлалов GeekBrains", xOffset - 1, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
 
 	}
 }
