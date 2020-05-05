e1464d8a38349a8cae0019c16a1f42cb435e7e5a
Superstas91 <superstas91@gmail.com>
2020-05-05 12:50:26 +0300

    стены, полиморфизм

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -13,17 +12,10 @@ namespace Snake
 		{
 			Console.SetBufferSize(80, 25);
 
-			// Отрисовка рамки
-			HorizontallLine upLine = new HorizontallLine(0, 78, 0, '+');
-			HorizontallLine downLine = new HorizontallLine(0, 78, 24, '+');
-			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-			upLine.Draw();
-			downLine.Draw();
-			leftLine.Draw();
-			rightLine.Draw();
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
 
-			// Отрисовка точек
+			// Отрисовка точек			
 			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
 			snake.Draw();
@@ -32,30 +24,30 @@ namespace Snake
 			Point food = foodCreator.CreateFood();
 			food.Draw();
 
-			Console.CursorVisible = false;
 			while (true)
 			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
 				if (snake.Eat(food))
 				{
 					food = foodCreator.CreateFood();
 					food.Draw();
 				}
-				else 
+				else
 				{
 					snake.Move();
 				}
 
 				Thread.Sleep(100);
-
 				if (Console.KeyAvailable)
 				{
 					ConsoleKeyInfo key = Console.ReadKey(true);
 					snake.HandleKey(key.Key);
-				}	
+				}
 			}
-			
 		}
-
 
 	}
 }
