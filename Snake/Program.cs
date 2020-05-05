b0c3c52969b84c62bdb3be37333acdff1ea6bc6c
Superstas91 <superstas91@gmail.com>
2020-05-05 09:25:34 +0300

    управление змейкой, инкапсуляция (HandleKey)

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -18,31 +18,29 @@ namespace Snake
 			HorizontallLine downLine = new HorizontallLine(0, 78, 24, '+');
 			VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
 			VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-			upLine.Drow();
-			downLine.Drow();
-			leftLine.Drow();
-			rightLine.Drow();
+			upLine.Draw();
+			downLine.Draw();
+			leftLine.Draw();
+			rightLine.Draw();
 
 			// Отрисовка точек
 			Point p = new Point(4, 5, '*');
 			Snake snake = new Snake(p, 4, Direction.RIGHT);
-			snake.Drow();
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
-			snake.Move();
-			Thread.Sleep(300);
+			snake.Draw();
+
+			Console.CursorVisible = false;
+
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey(true);
+					snake.HandleKey(key.Key);
+				}
+				Thread.Sleep(100);
+				snake.Move();
+			}
+			
 		}
 
 
